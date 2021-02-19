08343509edbc82debde0b539f8ebf452f1ee0538
YukiNishigaki99 <yukinishigaki@byui.edu>
2021-02-19 18:51:25 +0900

    initial commit

--- js/discount.js
+++ js/discount.js
@@ -6,16 +6,23 @@ function discount() {
     //processing: calculate the customer's discount if they buy in Tuesday or Wednesday.
     let total;
     if (subtotal > 50 && dayOfWeek == 2) {
-        total = subtotal * .9;
+        total = subtotal * 0.9;
     }
     else if (subtotal > 50 && dayOfWeek == 3) {
-        total = subtotal * .9;
+        total = subtotal * 0.9;
     }
     else {
-        total = subtotal
+        total = subtotal;
     }
 
     //output: the total amount after discount and adding tax
     totalAfterTax = total * 1.06
     document.getElementById('output').innerHTML = '$' + totalAfterTax
 }
+
+
+
+//Monday (1)	25.95	27.51
+//Tuesday (2)	37.00	39.22
+//Wednesday (3)	53.25	50.80
+//Thursday (4)	72.81	77.18
