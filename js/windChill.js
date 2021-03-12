fcd2b3307322e68e12b2b16c27784b31b2f51540
YukiNishigaki99 <yukinishigaki@byui.edu>
2021-03-12 19:20:17 +0900

    initial commit

--- js/windChill.js
+++ js/windChill.js
@@ -7,10 +7,10 @@ function doInputOutput() {
     let windspeed = parseFloat(document.getElementById('windspeed').value);
     if (temperature < 50 && windspeed >= 3) {
         let windchill = windChill(temperature, windspeed);
-        document.getElementById('output').innerHTML = windchill;
+        document.getElementById('output').innerHTML = `${windchill.toFixed(0)} (°F)`;
     }
 }
 
 function windChill(tempF, speed) {
-    return 35.74 + 0.6215 * tempF - 35.75 * Math.power(speed, 0.16) + 0.4275 * tempF * Math.power(speed, 0.16);
+    return 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
 }
