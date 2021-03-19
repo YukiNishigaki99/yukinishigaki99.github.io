07c0bfd986234ad268497dd99846aa82e494b858
YukiNishigaki99 <yukinishigaki@byui.edu>
2021-03-19 20:37:23 +0900

    initial commit

--- js/loan.js
+++ js/loan.js
@@ -1,4 +1,4 @@
-function doPayment ( ) {
+function doPayment() {
     // Get inputs from the user.
     let principal = parseFloat(document.getElementById('principal').value);
     let annualRate = parseFloat(document.getElementById('annualRate').value);
@@ -6,35 +6,35 @@ function doPayment ( ) {
     let paymentsPerYear = parseFloat(document.getElementById('paymentsPerYear').value);
 
     // Call the function.
-    let payment = computePayment (principal, annualRate, numberOfYears, paymentsPerYear);
+    let payment = computePayment(principal, annualRate, numberOfYears, paymentsPerYear);
 
     // Display a result to the user.
-    document.getElementById('payment').innerHTML = `$${payment.tofixed(2)}`;
+    document.getElementById('payment').innerHTML = `$${payment.toFixed(2)}`;
 }
 
-function doBalance ( ) {
+function doBalance() {
     // Get inputs from the user.
     let principal = parseFloat(document.getElementById('principal').value);
     let annualRate = parseFloat(document.getElementById('annualRate').value);
     let numberOfYears = parseFloat(document.getElementById('numberOfYears').value);
     let paymentsPerYear = parseFloat(document.getElementById('paymentsPerYear').value);
-    let numberOfPaymentPaidToDate = parseFloat(document.getElementById('numberOfPaymentPaidToDate').value);
+    let numberOfPaymentPaidToDate = parseFloat(document.getElementById('numberOfPaymentsPaidToDate').value);
 
     // Call the function.
-    let balance = computeBalance (principal, annualRate, numberOfYears, paymentsPerYear, numberOfPaymentPaidToDate);
+    let balance = computeBalance(principal, annualRate, numberOfYears, paymentsPerYear, numberOfPaymentPaidToDate);
 
     // Display a result to the user.
-    document.getElementById('balance').innerHTML = `$${balance.tofixed(2)}`;
+    document.getElementById('balance').innerHTML = `$${balance.toFixed(2)}`;
 }
 
-function computePayment (principal, annualRate, years, periodsPerYear) {
+function computePayment(principal, annualRate, years, periodsPerYear) {
     let r = annualRate / periodsPerYear;
     let n = years * periodsPerYear;
     let p = principal * r / 1 - Math.pow((1 + r, -n));
     return p;
 }
 
-function computeBalance (principal, annualRate, years, periodsPerYear, numberOfPaymentPaidToDate) {
+function computeBalance(principal, annualRate, years, periodsPerYear, numberOfPaymentPaidToDate) {
     let p = computePayment(principal, annualRate, years, periodsPerYear);
     let r = annualRate / periodsPerYear;
     let balance = principal * Math.pow((1 + r), numberOfPaymentPaidToDate) - (p * (Math.pow((1 + r), numberOfPaymentPaidToDate) - 1) / r);
